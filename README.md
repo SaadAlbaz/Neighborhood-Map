d730bec6db2566633e7f0ef7137c67b39f2c330d
Saad <sirsaad11@gmail.com>
2017-06-03 20:54:35 +0300

    bugs in shownig ratings from foursquare api

---
+++ README.md
@@ -0,0 +1,3 @@
+# How to run
+1. Download the project
+2. Open index.html
--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 // Create an arrya of locations with titles and markers
 var locations = [
-    {title: 'Alchemy coffee', location: {lat: 24.80652, lng: 46.650542}, marker:''},
-    {title: 'Camel step coffee', location: {lat: 24.769306, lng: 46.691027}, marker:''},
-    {title: 'Manual brew coffee', location: {lat: 24.772537, lng: 46.710265}, marker:''},
-    {title: 'coffee tale', location: {lat:24.782176, lng: 46.706268}, marker:''},
-    {title: 'Five Elephants', location: {lat: 24.81698, lng: 46.644359}, marker:''},
-    {title: 'The Roasting house', location: {lat: 24.743643, lng: 46.674513}, marker:''}
+    {title: 'Alchemy coffee', location: {lat: 24.80652, lng: 46.650542}, marker:'', id:'58baa0f60b5656702e53bdee', info:''},
+    {title: 'Camel step coffee', location: {lat: 24.769306, lng: 46.691027}, marker:'', id:'53adccbd498ec970f3c9e074', info:''},
+    {title: 'Manual brew coffee', location: {lat: 24.772537, lng: 46.710265}, marker:'', id:'58b3acebf22995176a0fe73d', info:''},
+    {title: 'coffee tale', location: {lat:24.782176, lng: 46.706268}, marker:'', id:'57929662498e5988d3b1fd36', info:''},
+    {title: 'Five Elephants', location: {lat: 24.81698, lng: 46.644359}, marker:'', id:'5884fddf0037eb3b6cbd3d36', info:''},
+    {title: 'The Roasting house', location: {lat: 24.743643, lng: 46.674513}, marker:'', id:'53419640498e503a1014d54b', info:''}
 ];
 
 
  var map;
  var bounds;
-
+ var infoWindowI;
  var initMap =function(){
      // start maps on the "map" div
        map = new google.maps.Map(document.getElementById('map'), {
@@ -26,6 +26,9 @@ var locations = [
        for (var i = 0; i < locations.length; i++) {
            var position = locations[i].location;
            var title = locations[i].title;
+           var id = locations[i].id;
+           var rating;
+           var markerContent;
            var marker = new google.maps.Marker({
                 map: map,
                 position: position,
@@ -34,29 +37,46 @@ var locations = [
           });
           // Push the marker to our locations array
           locations[i].marker = marker;
-    
+        //  markers.push(marker);
           // click event for the markers
-          marker.addListener('click', function() {
-            openInfo(this, infowindow);
+          //marker.addListener('click', toggleBounce);
+          var infowindow = new google.maps.InfoWindow({});
+          
+         $.ajax({url: "https://api.foursquare.com/v2/venues/"+locations[i].id+"?oauth_token=ENTUA10JFDOS0AD4C10GDHJQQHHHHBSLCD5FXQ5IIWTBV0QR&v=20170603", success: function(result){
+               rating = result.response.venue.rating;  
+               infowindow.setContent('<div>' + rating + '</div>');
+               console.log(infowindow)
+              marker.addListener('click', function() {
+               openInfo(this, infoWindow);
           });
-           bounds.extend(locations[i].marker.position);
+            }});
+           
+            locations[i].info = infowindow;
+         
+          bounds.extend(locations[i].marker.position);
        };
-        
         // Extend the boundaries of the map for each marker
         map.fitBounds(bounds);
+        
+        for( var i =0 ; i < locations.length; i++){
+          infoWindowI = locations[i].info;
+           locations[i].marker.addListener('click', function() {
+            openInfo(this, infoWindowI);
+          });
+        };
         
         function openInfo(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
             if (infowindow.marker != marker) {
                 infowindow.marker = marker;
-                infowindow.setContent('<div>' + marker.title + '</div>');
                 infowindow.open(map, marker);
                 // Make sure the marker property is cleared if the infowindow is closed.
                 infowindow.addListener('closeclick',function(){
                   infowindow.setMarker = null;
                 });
             }
-      }   
+      };
+
 };
 
 function renderMarkers(filterdLocations){
@@ -70,8 +90,8 @@ function renderMarkers(filterdLocations){
         filterdLocations[i].marker.setVisible(true);
         bounds.extend(filterdLocations[i].marker.position);
     };
-      // Extend the boundaries of the map for each marker
-      map.fitBounds(bounds);
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
 };
 
 var viewModel = function() {
@@ -102,7 +122,7 @@ var viewModel = function() {
             locations[i].marker.setVisible(true);
         };
         return true;
-    }
+    };
 
 };
 
